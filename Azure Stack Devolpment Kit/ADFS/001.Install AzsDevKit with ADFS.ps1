c0b153d5200861a1a362163e9d1bfed816877be7
lifeisju <lifeisju@gmail.com>
2017-11-21 18:00:09 +0900

    AzS

---
+++ Azure Stack Devolpment Kit/ADFS/001.Install AzsDevKit with ADFS.ps1
@@ -0,0 +1,3 @@
+﻿$adminpass = ConvertTo-SecureString '●●●●●●●●'-AsPlainText -Force
+cd C:\CloudDeployment\Setup
+.\InstallAzureStackPOC.ps1 -AdminPassword $adminpass -UseADFS -NatIPv4Subnet 192.168.0.0/24 -NatIPv4Address 192.168.0.151 -NatIPv4DefaultGateway 192.168.0.1 -DNSForwarder 168.126.63.1 -TimeServer 52.168.138.145
